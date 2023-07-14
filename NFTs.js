dad8c9c6281983a69c3b0bf61c4e1e15886c097d
yannaarmaza <139463771+yannaarmaza@users.noreply.github.com>
2023-07-14 13:08:59 +0800

    Create NFTs.js

---
+++ NFTs.js
@@ -0,0 +1,53 @@
+/*
+Assessment Requirements
+1. Create a variable that can hold a number of NFT's. What type of variable might this be?
+2. Create an object inside your mintNFT function that will hold the metadata for your NFTs. 
+   The metadata values will be passed to the function as parameters. When the NFT is ready, 
+   you will store it in the variable you created in step 1
+3. Your listNFTs() function will print all of your NFTs metadata to the console (i.e. console.log("Name: " + someNFT.name))
+4. For good measure, getTotalSupply() should return the number of NFT's you have created
+*/
+
+// create a variable to hold your NFT's
+const NFTs = []
+// this function will take in some values as parameters, create an
+// NFT object using the parameters passed to it for its metadata, 
+// and store it in the variable above.
+function mintNFT(_name, _eyeColor, _shirtType, _accessories) {
+    const NFT = {
+        "name":  _name,
+        "eyeColor": _eyeColor,
+        "shirtType": _shirtType,
+        "accessories": _accessories
+    }
+    NFTs.push (NFT);
+    console.log ("Minted: " + _name);
+}
+
+// create a "loop" that will go through an "array" of NFT's
+// and print their metadata with console.log()
+function listNFTs() {
+    for(let i = 0; i < NFTs.length; i++){
+        console.log("\nID \t\t\t\t" + (i + 1));
+        console.log("\nName: \t\t\t" + NFTs[i].name);
+        console.log("Eye Color: \t\t" + NFTs[i].eyecolor);
+        console.log("Shirt Type: \t" + NFTs[i].shirtType);
+        console.log("Accessories: \t" + NFTs[i].accessories);
+    }
+
+}
+
+// print the total number of NFTs we have minted to the console
+function getTotalSupply () {
+    console.log("\n" + NFTs.length);
+}
+
+// call your functions below this line
+mintNFT ("jen", "Brown", "Croptop", "Choker");
+mintNFT ("kat", "Black", "Sweatshirt", "Gold Chains");
+mintNFT ("jom", "Black", "Hoodie", "Ring");
+mintNFT ("cad", "Brown", "Suit", "Necklace");
+mintNFT ("mor", "Brown", "Croptop", "Necklace");
+listNFTs();
+getTotalSupply();
+
